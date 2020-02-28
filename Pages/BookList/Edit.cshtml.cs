66661b788cf06e04a9191e5e6b1797d3e867ae47
onuchukwuchika <chuzksy@gmail.com>
2020-02-28 12:14:27 +0100

    add edit post on the book

--- Pages/BookList/Edit.cshtml.cs
+++ Pages/BookList/Edit.cshtml.cs
@@ -22,9 +22,26 @@ namespace BookListRazor.Pages.BookList
             _dbContext = dbContext ??
                 throw new ArgumentNullException (nameof (dbContext));
         }
-        public async Task OnGet (Guid id)
+        public async Task OnGet (Guid bookId)
         {
-            Book = await _dbContext.Books.FindAsync(id);
+            Book = await _dbContext.Books.FindAsync(bookId);
+        }
+
+        public async Task<IActionResult> OnPost ()
+        {
+            if(ModelState.IsValid)
+            {
+                var bookFromDb = await _dbContext.Books.FindAsync(Book.Id);
+                bookFromDb.Name = Book.Name;
+                bookFromDb.ISBN = Book.ISBN;
+                bookFromDb.Author = Book.Author;
+
+                await _dbContext.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+
+            return RedirectToPage();
         }
     }
 }
